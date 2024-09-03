c971207dd1213be5338c834dd4dae6a991450857
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2024-09-03 14:43:07 +0530

    fix: deploy issue regarding version

--- test.jsx
+++ test.jsx
@@ -15,3 +15,23 @@
 //   "react-icons": "^5.3.0",
 //   "react-router-dom": "6.17.0"
 // }
+
+// 2nd
+
+// "dependencies": {
+//   "@emotion/react": "^11.13.3",
+//   "@emotion/styled": "^11.13.0",
+//   "@heroicons/react": "2.0.18",
+//   "@material-tailwind/react": "^2.1.9",
+//   "@mui/icons-material": "^5.16.7",
+//   "@mui/material": "^5.16.7",
+//   "apexcharts": "3.44.0",
+//   "axios": "^1.7.5",
+//   "mui-datatables": "^4.3.0",
+//   "react": "^18.3.1",
+//   "react-apexcharts": "1.4.1",
+//   "react-dom": "^18.3.1",
+//   "react-hot-toast": "^2.4.1",
+//   "react-icons": "^5.3.0",
+//   "react-router-dom": "6.17.0"
+// }
